61d8465ded45b94bb485e1c0dd632f76d7505bbc
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-26 22:54:34 -0300

    Instalando o RealmDB no projeto

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
+import { REALM_APP_ID } from '@env';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { AppProvider, UseProvider } from '@realm/react';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from "styled-components";
 import { Loading } from "./src/components/Loading";
+import { Home } from './src/screens/home';
 import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/theme";
 
@@ -16,10 +19,15 @@ export default function App() {
   }
 
   return (
+    <AppProvider id={REALM_APP_ID}>
     <ThemeProvider theme={theme}>
+      
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-     <SignIn/>
+      <UseProvider fallback={SignIn}>
+        <Home/>
+      </UseProvider>    
      </ThemeProvider>
+     </AppProvider>
   );
 }
 
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,8 +1,43 @@
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import backIMG from '../../assets/background.png';
 import { Button } from '../../components/button';
 import { Container, Slogan, Title } from './styles';
 
+GoogleSignin.configure({
+  scopes:['email', 'profile'],
+  webClientId:WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+  
+})
+
 export function SignIn() {
+
+  const [isAuthenticating , setIsAuthenticating] = useState(false)
+
+  async function handleGoogleSignin(){
+    try {
+      setIsAuthenticating(true)
+
+      const {idToken} = await GoogleSignin.signIn()
+      
+      if(idToken){
+
+      }else{
+        Alert.alert("Entrar", "Você precisa permitir o acesso ao seu Google")
+        setIsAuthenticating(false)
+        return
+      }
+
+    } catch (error) {
+      console.log(error)
+      setIsAuthenticating(false)
+      Alert.alert("Entrar", "Não foi possível conectar-se a sua conta ")
+    }
+  }
+
   return (
     <Container source={backIMG}>
       <Title>Ignite Fleet </Title>
@@ -11,7 +46,7 @@ export function SignIn() {
         Gestão de veículos
       </Slogan>
 
-      <Button title="Entrar com o google"/>
+      <Button title="Entrar com o google" isLoading={isAuthenticating} onPress={handleGoogleSignin}/>
     </Container>
   );
 }
