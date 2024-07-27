634b18e658b2eb36ffe3687d419e18aa4b2a8b31
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-27 17:44:57 -0300

    Criando input para placa do veículo

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Header } from '../../components/Header'
-import { Container } from './styles'
+import { LicensePlateInput } from '../../components/LicensePlateInput'
+import { Container, Content } from './styles'
 
 export function Departure() {
   return (
     <Container>
       <Header title='Saída'/>
+
+    <Content>
+      <LicensePlateInput label='Placa do veículo' placeholder='BRA124'/>
+    </Content>
     </Container>
   )
 }
