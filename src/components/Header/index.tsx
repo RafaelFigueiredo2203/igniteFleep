3812d9fa3b0148e392946771a6d1e0759a7f1f52
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-27 17:33:42 -0300

    Criando o Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { useNavigation } from '@react-navigation/native'
+import { ArrowLeft } from 'phosphor-react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useTheme } from 'styled-components'
+import { Container, Title } from './styles'
+
+type Props ={
+  title: string;
+}
+
+export function Header({title}: Props) {
+  const {COLORS} = useTheme()
+  const insets = useSafeAreaInsets()
+  const {goBack} = useNavigation()
+
+  const paddingTop = insets.top + 42
+
+  return (
+    <Container style={{paddingTop}}>
+      <TouchableOpacity activeOpacity={0.7}  onPress={goBack}>
+        <ArrowLeft size={24} weight='bold' color={COLORS.BRAND_LIGHT}/>
+      </TouchableOpacity>
+
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  )
+}
--- src/screens/Departure/index.ts
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Container } from './styles'
-
-export function Departure() {
-  return (
-    <Container></Container>
-  )
-}
