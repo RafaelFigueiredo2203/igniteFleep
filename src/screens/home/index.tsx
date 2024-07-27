3a3e8b4b00a1fe5481637441c0e2c8ec6d0fb37b
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-27 17:06:46 -0300

    Criando o componente car status

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,16 @@
 import { Container } from './styles';
 
+import { CarStatus } from '../../components/CarStatus';
 import { HomeHeader } from '../../components/HomeHeader';
 
 export function Home() {
   return (
     <Container>
       <HomeHeader />
+
+      <Content>
+      <CarStatus/>
+      </Content>
     </Container>
   );
 }
