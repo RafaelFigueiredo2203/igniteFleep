88ab0a894bad07ca6cfc738fe1812e10263d24af
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-08-06 21:59:44 -0300

    Cancelar o veículo em uso

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,6 +1,14 @@
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { X } from 'phosphor-react-native';
 import React from 'react';
-import { Container } from './styles';
+import { Alert } from 'react-native';
+import { BSON } from 'realm';
+import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { Header } from '../../components/Header';
+import { useObject, useRealm } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
+import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
 
 type RouteParamsProps = {
   id:string
@@ -11,10 +19,58 @@ export function Arrival() {
   const route = useRoute();
   const {id} = route.params as RouteParamsProps
 
-  console.log(id)
+  const historic = useObject(Historic, new BSON.UUID(id))
+  const realm = useRealm()
+  const {goBack} = useNavigation()
+
+  function handleRemoveVehicleUsage(){
+    Alert.alert(
+      'Cancelar',
+      'Cancelar a utilização do veículo?',
+      [
+        {text: 'Não', style: 'cancel'},
+        {text:'Sim', onPress:() =>  removedVehicleUsage()}
+      ]
+    )
+  }
+
+  function removedVehicleUsage(){
+    realm.write(() => {
+      realm.delete(historic);
+    });
+    goBack()
+  }
+
+
   return (
     <Container>
+      <Header title="Chegada"/>
+      <Content>
+      <Label>
+      Placa do veículo
+      </Label>
 
+      <LicensePlate>
+        {historic?.license_plate}
+      </LicensePlate>
+
+      <Label>
+      Finalidade
+      </Label>
+
+      <Description>
+      {historic?.description}
+      </Description>
+
+      <Footer>
+        <ButtonIcon
+        icon={X}
+        onPress={handleRemoveVehicleUsage}
+        />
+        <Button title="Registrar chegada"/>
+      </Footer>
+
+      </Content>
     </Container>
   )
 }
