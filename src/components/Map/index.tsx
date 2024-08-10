1fed5e31fc6774484549171afaebb6b81ccf7812
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-08-10 18:44:48 -0300

    Desenhando a linha no mapa

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,7 @@
 import { Car, FlagCheckered } from 'phosphor-react-native';
-import React from 'react';
-import MapView, { LatLng, MapViewProps, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useRef } from 'react';
+import MapView, { LatLng, MapViewProps, Marker, Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useTheme } from 'styled-components/native';
 import { IconBox } from '../IconBox';
 
 type Props = MapViewProps & {
@@ -8,11 +9,23 @@ type Props = MapViewProps & {
 }
 
 export function Map({coordinates, ...rest}: Props) {
-
+  const {COLORS} = useTheme()
+  const mapRef = useRef<MapView>(null)
   const lastCoordinate = coordinates[coordinates.length - 1]
+
+  async function onMapLoaded(){
+    if(coordinates.length > 1){
+      mapRef.current?.fitToSuppliedMarkers(['departure', 'arrival'],{
+        edgePadding: {top: 50, bottom: 50, left: 50, right: 50},
+        animated: true,
+      })
+      
+    }
+  }
 
   return (
     <MapView
+    ref={mapRef}
     provider={PROVIDER_GOOGLE}
     style={{width:'100%', height:200}}
     region={{
@@ -21,10 +34,12 @@ export function Map({coordinates, ...rest}: Props) {
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}
+    onMapLoaded={onMapLoaded}
     {...rest}
     >
       <Marker
       coordinate={coordinates[0]}
+      identifier="departure"
       >
         <IconBox
         size="SMALL" icon={Car}
@@ -33,13 +48,22 @@ export function Map({coordinates, ...rest}: Props) {
 
       {
         coordinates.length > 1 && 
+        <>
         <Marker
+        identifier="departure"
         coordinate={lastCoordinate}
         >
         <IconBox
         size="SMALL" icon={FlagCheckered}
         />
         </Marker>
+
+        <Polyline
+        coordinates={[...coordinates]}
+        strokeColor={COLORS.GRAY_700}
+        strokeWidth={6}
+        />
+        </>
       }
     </MapView>
   )
